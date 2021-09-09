68954bd0a1c1ade8fb083d9348a700f12a82c4d3
Tarun <tarunsunny3@gmail.com>
2021-09-09 21:18:54 +0530

    changed http to https in api url

--- frontend/src/apiURL.js
+++ frontend/src/apiURL.js
@@ -1,2 +1,2 @@
-const API = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&`;
+const API = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&`;
 export default API; 
